a60525204ac251106e33bc0a9230c1cb3ea2fe04
demon-i386 <71338746+EtoYoshio@users.noreply.github.com>
2020-11-07 19:12:40 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+/* === compara 2 dados levando em conta seu tipo,
+ignorando qualquer desigualdade como tal 
+(igualdade estrita) compara dois valores para a igualdade
+, não passando por nenhuma conversão, como por
+exemplo comparação entre null e undefined, ao utilizar
+== eles são a mesma coisa, mas não utilizando ===,
+ou seja, tipo diferente = false, ao contrário 
+
+mesma coisa utilizando !==, mas ao contrário claro
+
+Object.is(x, y) determina se dois valores tem o mesmo
+valor */
